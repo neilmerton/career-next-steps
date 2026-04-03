8024a3f26ff6220e0ad6775530ea20a5d6e3cf78
Neil Merton <neilmerton@gmail.com>
2026-04-03 08:57:30 +0100

    feat: add pwa iso detection

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import type { Metadata } from 'next'
 import './globals.css'
 
+const UA = navigator.userAgent;
+
+const IOS = UA.match(/iPhone|iPad|iPod/);
+const ANDROID = UA.match(/Android/);
+
+export const PLATFORM = IOS ? 'ios' : ANDROID ? 'android' : 'unknown';
+
+const standalone = global.window && window.matchMedia('(display-mode: standalone)').matches;
+
+export const INSTALLED = !!(standalone || (IOS && !UA.match(/Safari/)));
+
 export const metadata: Metadata = {
   title: {
     template: '%s | Career Next Steps',
@@ -16,7 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={INSTALLED ? "pwa-ios" : ""}>{children}</body>
     </html>
   )
 }
