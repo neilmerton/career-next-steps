9d51abb3f86b0e27fb7ffc96226b79382094eb2b
Neil Merton <neilmerton@gmail.com>
2026-04-09 13:53:32 +0100

    feat: extend staleTime for all queries to 5 minutes

--- lib/queries/get-query-client.ts
+++ lib/queries/get-query-client.ts
@@ -4,7 +4,7 @@ function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 60 * 1000,
+        staleTime: 5 * 60 * 1000, // 5 minutes
       },
       dehydrate: {
         shouldDehydrateQuery: (query) =>
